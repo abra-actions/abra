c46d2a1b583e6e24ebd328c0a4a3d3c863c564ab
Jake Medeiros <jakemedeiros@juzumaru.lan>
2025-03-12 18:31:09 -0400

    delete test

--- testExecutor.js
+++
@@ -1,8 +0,0 @@
-import { executeAction } from "./src/core/executor.js";
-
-(async () => {
-    const result = await executeAction("createUser", { 
-        user: { name: "Jake", email: "jake@example.com", age: 30 } 
-    });
-    console.log("Execution Result:", result);
-})();
