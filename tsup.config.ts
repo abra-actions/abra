0432026fb9963c6fc884d3135a9e6ed28c956b7f
Jake Medeiros <jakemedeiros@Mac.lan>
2025-04-12 17:46:11 -0400

    use tsup for bundling

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'node18',
+  outDir: 'dist',
+  bundle: true,
+  clean: true,
+  platform: 'node',
+});
+
