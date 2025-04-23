08312a590d9864f3023d88f3a54836b9e78812e7
Jake Medeiros <jakejmedeiros@gmail.com>
2025-04-22 20:36:04 -0400

    cjs + chmod in build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/AbraAssistant.tsx'],
-  format: ['esm'],
+  format: ['cjs'],
   dts: true,
   splitting: false,
   sourcemap: true,
