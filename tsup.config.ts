075935f550ec832b7e8101b3ae042c7e298cfbba
Jake Medeiros <jakejmedeiros@gmail.com>
2025-04-29 02:58:08 -0400

    clean up config/package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,5 @@ export default defineConfig({
   sourcemap: true,
   dts: true,
   clean: true,
-  splitting: false, 
-  minify: false     
+  external: ['fs', 'path', 'os', 'crypto', 'perf_hooks', 'inspector'],
 });
